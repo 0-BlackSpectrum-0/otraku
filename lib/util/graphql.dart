e6ccd7941405186accda954e96159a8237db543f
BlackSpectrum <79910450+0-BlackSpectrum-0@users.noreply.github.com>
2026-05-20 20:49:06 +0530

    notification logo

--- lib/util/graphql.dart
+++ lib/util/graphql.dart
@@ -713,6 +713,10 @@ abstract class GqlQuery {
             id
             type
             activityId
+            context
+            activity{
+            ... on TextActivity { text }
+            }
             user {id name avatar {large}}
             createdAt
           }
@@ -720,6 +724,8 @@ abstract class GqlQuery {
             id
             type
             activityId
+            context
+            message { message }
             user {id name avatar {large}}
             createdAt
           }
@@ -734,6 +740,11 @@ abstract class GqlQuery {
             id
             type
             activityId
+            context
+            activity {
+            ... on TextActivity { text }
+            ... on MessageActivity{ message }
+            }
             user {id name avatar {large}}
             createdAt
           }
@@ -771,15 +782,16 @@ abstract class GqlQuery {
             type
             context
             thread {title}
-            comment {id siteUrl}
+            comment {id siteUrl comment}
             user {id name avatar {large}}
             createdAt
           }
           ... on ThreadCommentMentionNotification {
             id
             type
+            context
             thread {title}
-            comment {id siteUrl}
+            comment {id siteUrl comment}
             user {id name avatar {large}}
             createdAt
           }
